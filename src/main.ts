316312f5c735683e48c5c7acb4f2895414485557
Mario Ibarra <mario.ibarra.86@gmail.com>
2024-07-22 09:05:38 -0500

    Considerando la configuracion de cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,12 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: {
+      origin: '*', // Permitir cualquier origen
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
